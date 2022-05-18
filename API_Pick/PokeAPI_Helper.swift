83cad16da7492bddf7d1c67ab005b390f0b9b060
kushal <kushal_vaghani@yahoo.com>
2022-05-18 18:14:38 -0400

    Initial Commit

---
+++ API_Pick/PokeAPI_Helper.swift
@@ -0,0 +1,35 @@
+//
+//  PokeAPI_Helper.swift
+//  API_Pick
+
+
+import Foundation
+
+class PokeAPI_Helper{
+    private static var baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")
+    
+    private static var session: URLSession = {
+        var config = URLSessionConfiguration.default
+        return URLSession(configuration: config)
+    }()
+    
+    static func fetchData(callback: @escaping (Any) -> Void){
+        let request = URLRequest(url: baseURL!)
+        let task = session.dataTask(with: request) {
+         data, response, error in
+            if let data = data {
+                do{
+                    let jsonData = try JSONSerialization.jsonObject(with: data, options: [])
+                    callback(jsonData)
+                }catch let error{
+                    print("Error \(error)")
+                }
+            } else if let error = error {
+                print(error)
+            } else {
+                print("Unknown Error")
+            }
+        }
+        task.resume()
+    }
+}
---
+++ API_Pick/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  API_Pick
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showPokemonData(_ sender: Any) {
+        PokeAPI_Helper.fetchData {
+            printString in
+           print(printString)
+        }
+    }
+    
+    @IBAction func showBreakingBadData(_ sender: Any) {
+        BreakinBad_Helper.fetchData {
+            printString in
+           print(printString)
+        }
+    }
+    
+    @IBAction func showGutendexData(_ sender: Any) {
+        Gutendex_Helper.fetchData {
+            printString in
+           print(printString)
+        }
+    }
+    
+}
+
